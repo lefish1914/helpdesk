ef8166d9557baeb61bd33800aab2aa466d318076
lefish1914 <leandroferreira99@gmail.com>
2023-07-26 11:40:13 -0300

    projeto iniciado

---
+++ src/main/java/com/valdir/helpdesk/HelpdesksApplication.java
@@ -0,0 +1,13 @@
+package com.valdir.helpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpdesksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpdesksApplication.class, args);
+	}
+
+}
